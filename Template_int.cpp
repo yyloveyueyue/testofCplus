90012c74a6b6a7ff4806947a14005a884405094f
yuan ye <974972165@qq.com>
2019-07-09 08:51:19 +0800

    类模板继承方法2

--- Template_int.cpp
+++ Template_int.cpp
@@ -8,15 +8,30 @@ public:
     T m_a;
 };
 
+// 继承类模板，必须要确定基类的大小
 class Child:public Father<int>
 {
 public:
     int m_b;
 };
 
+template<class T1, class T2>
+class Child2: public Father<T2>
+{
+public:
+    T1 m_c;
+};
+
+void test01(){
+    Child2<int, double> child; //由用户指定类型
+    cout << sizeof(child) << endl;
+
+}
+
+
 int main()
 {
-    
+    test01();
     system("pause");
     return EXIT_SUCCESS;
 }
